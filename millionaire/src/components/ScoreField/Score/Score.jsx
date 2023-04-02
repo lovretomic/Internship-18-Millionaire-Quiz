4f36373b269a51deb2179201e333a03ded0bf04b
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-04-03 00:50:24 +0200

    Updated personal score field

--- millionaire/src/components/ScoreField/Score/Score.jsx
+++ millionaire/src/components/ScoreField/Score/Score.jsx
@@ -1,9 +1,12 @@
 import classes from './index.module.css';
+import { useStepStatus } from 'providers/StepProvider';
+import steps from '../../../data/steps.json';
 
 const Score = () => {
+  const {step} = useStepStatus();
   return <div>
     <p className={classes.scoreTitle}>Your Score</p>
-    <div className={classes.scoreBar}>$ 0</div>
+    <div className={classes.scoreBar}>$ {step === 0 ? '0' : steps[step - 1]}</div>
   </div>
 }
 
