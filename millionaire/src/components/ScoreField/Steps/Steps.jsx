665beb46c91e3f5ce70e3e6148ade4e30a99b90e
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-04-02 23:36:50 +0200

    Imported clsx and updated steps field

--- millionaire/src/components/ScoreField/Steps/Steps.jsx
+++ millionaire/src/components/ScoreField/Steps/Steps.jsx
@@ -2,13 +2,17 @@ import classes from './index.module.css';
 import steps from '../../../data/steps.json';
 
 import { useStepStatus } from 'providers/StepProvider';
+import clsx from 'clsx';
 
 const Steps = () => {
   const { step } = useStepStatus();
   console.log('currentStep', step);
   return <div className={classes.steps}>
     {steps.map((stepText, i) => {
-      return <div key={i} className={`${classes.step} ${i === step ? "active" : ""}`}>
+      return <div key={i} className={clsx({
+        [classes.step]: true,
+        [classes.active]: i === step
+      })}>
         <div className={classes.stepNumber}>{i + 1}</div>
         <div className={classes.stepValue}>${stepText}</div>
       </div>
