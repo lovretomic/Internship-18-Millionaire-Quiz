8a8eb0bb4e91b7044fa801ab31f29c49e9c5742f
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-04-03 19:49:32 +0200

    Added 50:50 joker

--- millionaire/src/components/ScoreField/Jokers/Jokers.jsx
+++ millionaire/src/components/ScoreField/Jokers/Jokers.jsx
@@ -1,10 +1,46 @@
 import classes from './index.module.css';
+import { useQuestionStatus } from 'providers/QuestionProvider';
+import { useState } from 'react';
 
 const Jokers = () => {
+  const {question, setQuestion} = useQuestionStatus();
+
+  const defaultJokers = {
+    fiftyFifty: false,
+    phone: false,
+    public: false
+  }
+  const [usedJokers, setUsedJokers] = useState(defaultJokers);
+
+  const removeWrongAnswers = () => {
+    console.log(usedJokers);
+    setUsedJokers({...usedJokers, fiftyFifty: true});
+    alert('hanged');
+    const newOptions = [...question.options];
+    const answerIndex = newOptions.indexOf(question.answer);
+    const removedOptions = [];
+
+    while (removedOptions.length < 2) {
+      const randomNum = Math.floor(Math.random() * newOptions.length);
+      if (randomNum !== answerIndex && !removedOptions.includes(randomNum)) {
+        removedOptions.push(randomNum);
+      }
+    }
+
+    removedOptions.forEach((index) => {
+      newOptions[index] = '';
+    })
+
+    setQuestion({
+      ...question,
+      options: newOptions
+    });
+  }
+
   return <div className={classes.jokers}>
-    <button className={classes.jokerButton}>1</button>
-    <button className={classes.jokerButton}>2</button>
-    <button className={classes.jokerButton}>3</button>
+    <button className={classes.jokerButton} onClick={removeWrongAnswers} disabled={usedJokers.fiftyFifty}>50:50</button>
+    <button className={classes.jokerButton}>Phone</button>
+    <button className={classes.jokerButton}>Public</button>
   </div>;
 }
 
