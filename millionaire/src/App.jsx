5520b717d400a749633a60f822bf00209754cce2
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-04-04 00:44:10 +0200

    Added dialog provider

--- millionaire/src/App.jsx
+++ millionaire/src/App.jsx
@@ -1,31 +1,34 @@
-import classes from './App.module.css';
+import classes from "./App.module.css";
 
-import StepProvider from 'providers/StepProvider';
-import QuestionProvider from 'providers/QuestionProvider';
+import StepProvider from "providers/StepProvider";
+import QuestionProvider from "providers/QuestionProvider";
+import DialogProvider from "providers/DialogProvider";
 
-import Header from 'components/Header';
-import QuestionField from 'components/QuestionField';
-import QuestionButtons from 'components/QuestionButtons';
-import ScoreField from 'components/ScoreField';
+import Header from "components/Header";
+import QuestionField from "components/QuestionField";
+import QuestionButtons from "components/QuestionButtons";
+import ScoreField from "components/ScoreField";
 
 function App() {
   return (
-  <StepProvider>
-    <QuestionProvider>
-      <div className={classes.App}>
-        <Header></Header>
-        <div className={classes.content}>
-          <div className={classes.contentLeft}>
-            <QuestionField></QuestionField>
-            <QuestionButtons></QuestionButtons>
+    <StepProvider>
+      <QuestionProvider>
+        <DialogProvider>
+          <div className={classes.App}>
+            <Header></Header>
+            <div className={classes.content}>
+              <div className={classes.contentLeft}>
+                <QuestionField></QuestionField>
+                <QuestionButtons></QuestionButtons>
+              </div>
+              <div className={classes.contentRight}>
+                <ScoreField></ScoreField>
+              </div>
+            </div>
           </div>
-          <div className={classes.contentRight}>
-            <ScoreField></ScoreField>
-          </div>
-        </div>
-      </div>
-    </QuestionProvider>
-  </StepProvider>
+        </DialogProvider>
+      </QuestionProvider>
+    </StepProvider>
   );
 }
 
---
+++ millionaire/src/providers/DialogProvider.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext } from "react"
+
+export const DIALOG_TYPE = {
+  CONFIRM_ANSWER_DIALOG: 'CONFIRM_ANSWER_DIALOG',
+  PHONE_JOKER_DIALOG: 'PHONE_JOKER_DIALOG',
+  PUBLIC_JOKER_DIALOG: 'PUBLIC_JOKER_DIALOG'
+}
+
+const defaultContext = {
+  activeDialog: null,
+  additionalProps: {},
+  open: () => {},
+  close: () => {}
+}
+
+export const DialogContext = createContext(defaultContext);
+
+const DialogProvider = ({ children }) => {
+  const [activeDialog, setActiveDialog] = useState(null);
+  const [additionalProps, setAdditionalProps] = useState({});
+
+  const open = (dialog, additionalProps = {}) => {
+    setActiveDialog(dialog);
+    setAdditionalProps(additionalProps);
+  }
+
+  const close = () => {
+    setActiveDialog(null);
+    setAdditionalProps({});
+  }
+
+  return (
+  <DialogProvider.Provider value={{activeDialog, additionalProps}}>
+    {children}
+  </DialogProvider.Provider>
+  )
+}
+
+export const useDialog = () => useContext(DialogContext);
+
+export default DialogProvider;
