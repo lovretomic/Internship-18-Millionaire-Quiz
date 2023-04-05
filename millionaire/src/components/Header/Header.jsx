543806d6bfc94e8ef96a6ab0a14a303b1db4443e
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-04-05 17:42:42 +0200

    Updated header

--- millionaire/src/components/Header/Header.jsx
+++ millionaire/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import classes from './index.module.css';
 
 const Header = () => {
-  return <header className={classes.Header}>Who wants to win a million</header>
+  return <header className={classes.Header}><h1>Who wants to win a million</h1></header>
 }
 
 export default Header;
